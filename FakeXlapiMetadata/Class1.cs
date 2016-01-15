8a07014ffdf917e8cfa9ad1f9e39eb6a296097a1
Shaofeng Zhu <shaofengzhu@gmail.com>
2016-01-14 19:08:51 -0800

    Add a test file

---
+++ FakeXlapiMetadata/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.OfficeExtension.CodeGen;
+namespace FakeXlapi
+{
+    public interface Workbook
+    {
+        Worksheet ActiveWorksheet { get; }
+    }
+
+    public interface Worksheet
+    {
+        Range Range(string address);
+    }
+
+    public interface Range
+    {
+        string Text { get; set; }
+    }
+}
