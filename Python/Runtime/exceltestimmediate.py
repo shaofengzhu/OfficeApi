2431ec974cbcf451887e19cb407f7d6635fac41b
Shaofeng Zhu <shaozhu@microsoft.com>
2016-11-01 12:03:46 -0700

    Update immediate test

--- Python/Runtime/exceltestimmediate.py
+++ Python/Runtime/exceltestimmediate.py
@@ -1,10 +1,15 @@
-import runtime
+﻿import runtime
 import excel
 
 def simpleRangeTest():
-    context = excel.RequestContext("http://localhost:8052", runtime.RequestExecutionMode.immediateAndSlow)
-    r = context.workbook.worksheets.getItem("Sheet1").getRange("A1:B2")
+    context = excel.RequestContext("http://localhost:8052")
+    context.executionMode = runtime.RequestExecutionMode.immediateAndSlow
+    sheet = context.workbook.worksheets.getItem("Sheet1")
+    sheet.getUsedRange().clear()
+    r = sheet.getRange("A1:B2")
+    r.values = [["Hello", "World"], [1234, "=A2 + 1000"]]
     print(r.values)
+    t = sheet.tables.add(r, True)
 
 if __name__ == "__main__":
     simpleRangeTest()
