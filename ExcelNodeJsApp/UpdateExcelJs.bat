806adf35bc0e0c6b1e0dc34c8f78d5fe9b2861d8
Shaofeng Zhu <shaofengzhu@gmail.com>
2016-11-21 12:37:39 -0800

    Rename file

--- ExcelNodeJsApp/UpdateExcelJs.bat
+++ ExcelNodeJsApp/UpdateExcelJs.bat
@@ -1,6 +1,3 @@
-type \\shaozhu-hp800\Office\Target\x64\debug\osfclient\x-none\Office.Runtime.js > OfficeExtension.js
-type OfficeExtension.post.txt >> OfficeExtension.js
+copy \\shaozhu-hpvm8\Office\Target\x64\debug\osfclient\x-none\modules\office.runtime.js office.runtime.js
+copy \\shaozhu-hpvm8\Office\Target\x64\debug\xlshared\x-none\modules\excel.js excel.js
 
-type Excel.pre.txt > Excel.js
-type \\shaozhu-hp800\Office\Target\x64\debug\xlshared\x-none\Excel.js >> Excel.js
-type Excel.post.txt >> Excel.js
